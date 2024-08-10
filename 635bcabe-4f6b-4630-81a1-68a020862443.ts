4f9d7779845f319a244c921e8ed8b14b6dbddc46
Your Name <you@example.com>
2024-08-10 19:52:09 +0800

    1

---
+++ 635bcabe-4f6b-4630-81a1-68a020862443.ts
@@ -0,0 +1,60 @@
+export default {
+  "entity": {
+    "id": "635bcabe-4f6b-4630-81a1-68a020862443",
+    "name": "康生、伯达、春桥在中央整党建党工作座谈会的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "康生 陈伯达 张春桥",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/12.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/13.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/14.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/15.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/16.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/17.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/635bcabe-4f6b-4630-81a1-68a020862443/18.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "康生、伯达、春桥在中央整党建党工作座谈会的讲话",
+        "authors": [
+          "康生",
+          "陈伯达",
+          "张春桥"
+        ],
+        "page_start": 1,
+        "page_end": 18,
+        "dates": [
+          {
+            "year": 1970,
+            "month": 4,
+            "day": 17
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "635bcabe-4f6b-4630-81a1-68a020862443",
+  "resource_type": "book",
+  "version": 2
+}
