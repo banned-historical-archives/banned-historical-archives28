d37bde6e4dca5540fe1baac94b80664f6dc18653
Your Name <you@example.com>
2024-08-10 20:28:20 +0800

    1

---
+++ a66fc4ba-6cd5-468c-bdea-0255b729ed6c.ts
@@ -0,0 +1,38 @@
+export default {
+  "entity": {
+    "id": "a66fc4ba-6cd5-468c-bdea-0255b729ed6c",
+    "name": "“四人帮”及其余党在工业方面的反革命修正主义谬论摘编（初稿）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "上海市革委会工交组秘书组、宣传组",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/a66fc4ba-6cd5-468c-bdea-0255b729ed6c.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "“四人帮”及其余党在工业方面的反革命修正主义谬论摘编（初稿）",
+        "authors": [
+          "上海市革委会工交组秘书组、宣传组"
+        ],
+        "page_start": 1,
+        "page_end": 40,
+        "dates": [
+          {
+            "year": 1977,
+            "month": 2
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "a66fc4ba-6cd5-468c-bdea-0255b729ed6c.pdf",
+  "resource_type": "book",
+  "version": 2
+}
