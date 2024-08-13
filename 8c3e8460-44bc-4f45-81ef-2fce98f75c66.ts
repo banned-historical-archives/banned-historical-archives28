805446cd2c7a6144b4f8f3e542ed01c604c23668
Your Name <you@example.com>
2024-08-13 20:28:21 +0800

    1

--- 8c3e8460-44bc-4f45-81ef-2fce98f75c66.ts
+++ 8c3e8460-44bc-4f45-81ef-2fce98f75c66.ts
@@ -1,57 +1,57 @@
 export default {
-  "entity": {
-    "id": "8c3e8460-44bc-4f45-81ef-2fce98f75c66",
-    "name": "唐岐山同志在郑州市党员干部大会上的讲话",
-    "internal": false,
-    "type": "imgs",
-    "official": false,
-    "author": "唐岐山",
-    "files": [
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/1.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/2.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/3.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/4.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/5.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/6.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/7.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/8.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/9.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/10.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/11.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/12.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/13.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/14.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/15.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/16.jpg",
-      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/17.jpg"
-    ]
-  },
-  "parser_option": {
-    "articles": [
-      {
-        "title": "唐岐山同志在郑州市党员干部大会上的讲话",
-        "authors": [
-          "唐岐山"
-        ],
-        "page_start": 1,
-        "page_end": 17,
-        "dates": [
-          {
-            "year": 1974,
-            "month": 10,
-            "day": 12
-          }
-        ]
-      }
+  entity: {
+    id: '8c3e8460-44bc-4f45-81ef-2fce98f75c66',
+    name: '唐岐山同志在郑州市党员干部大会上的讲话',
+    internal: false,
+    type: 'img',
+    official: false,
+    author: '唐岐山',
+    files: [
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/1.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/2.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/3.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/4.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/5.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/6.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/7.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/8.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/9.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/10.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/11.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/12.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/13.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/14.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/15.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/16.jpg',
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives28/main/8c3e8460-44bc-4f45-81ef-2fce98f75c66/17.jpg',
     ],
-    "ocr": {
-      "use_onnx": true,
-      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
-      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
-    }
   },
-  "parser_id": "automation",
-  "path": "8c3e8460-44bc-4f45-81ef-2fce98f75c66",
-  "resource_type": "book",
-  "version": 2
-}
+  parser_option: {
+    articles: [
+      {
+        title: '唐岐山同志在郑州市党员干部大会上的讲话',
+        authors: [
+          '唐岐山',
+        ],
+        page_start: 1,
+        page_end: 17,
+        dates: [
+          {
+            year: 1974,
+            month: 10,
+            day: 12,
+          },
+        ],
+      },
+    ],
+    ocr: {
+      use_onnx: true,
+      det_model_dir: './paddle/onnx/ch_PP-OCRv4_det_infer.onnx',
+      rec_model_dir: './paddle/onnx/ch_PP-OCRv4_rec_infer.onnx',
+    },
+  },
+  parser_id: 'automation',
+  path: '8c3e8460-44bc-4f45-81ef-2fce98f75c66',
+  resource_type: 'book',
+  version: 2,
+};
